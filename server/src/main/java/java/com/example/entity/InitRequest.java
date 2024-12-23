2a311c57cfbf2d82040772231f65739a69e840e2
Vsevolod <vsevolod.brekelov@gmail.com>
2024-12-23 20:37:39 +0100

    add entity and schema

---
+++ server/src/main/java/java/com/example/entity/InitRequest.java
@@ -0,0 +1,22 @@
+package java.com.example.entity;
+
+public class InitRequest {
+//{
+//  "jsonrpc": "2.0",
+//  "id": 1,
+//  "method": "initialize",
+//  "params": {
+//    "protocolVersion": "2024-11-05",
+//    "capabilities": {
+//      "roots": {
+//        "listChanged": true
+//      },
+//      "sampling": {}
+//    },
+//    "clientInfo": {
+//      "name": "ExampleClient",
+//      "version": "1.0.0"
+//    }
+//  }
+//}
+}
